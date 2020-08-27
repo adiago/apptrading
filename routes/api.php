750ceb53d6bf968b5fb76a2e504b3d7ad780a4f7
Alexis Diago <alexis.diago@gmail.com>
2020-08-27 21:04:20 +0000

    permisos

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+
     //Route::middleware('auth:api')->group(function() {
     //    Route::get('/widgets-stats', 'StatisticsController@widgets');
     //    Route::get('/markets', 'MarketsController@index');
